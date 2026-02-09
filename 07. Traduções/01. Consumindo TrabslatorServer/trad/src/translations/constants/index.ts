2b1508bd068bab7c1c42b9a5783c8adca65b7f8f
AlexCol <eu_axil@yahoo.com.br>
2026-02-09 11:14:47 -0300

    .

--- 07. Traduções/01. Consumindo TrabslatorServer/trad/src/translations/constants/index.ts
+++ 07. Traduções/01. Consumindo TrabslatorServer/trad/src/translations/constants/index.ts
@@ -1,2 +1,3 @@
 const env = 'dev';
-export const BASE_URL = `http://localhost:3000/catalog/${env}/backend`;
+//export const BASE_URL = `http://localhost:3000/catalog/${env}/backend`;
+export const BASE_URL = `https://sys-tradutor.b-cdn.net/translations/${env}/backend/`;
